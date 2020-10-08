7529ae55b2ab69fb41236ba7be299c43f250d61d
testuser13407 <66666788+testuser13407@users.noreply.github.com>
2020-10-08 16:31:40 +0530

    Update deployAWS.groovy
    timeoutInMinutes to 50 from 30.

--- vars/deployAWS.groovy
+++ vars/deployAWS.groovy
@@ -47,7 +47,7 @@ def call(Map config) {
                              Version,
                              Environment,
                              NetworkStackName]
-                    , timeoutInMinutes: 40, pollInterval: 1000)
+                    , timeoutInMinutes: 50, pollInterval: 1000)
             return outputs
         }
     }
